caefb4785178d233b753febc4473e24b22e3ece7
cshekhar1337 <chander.shekhar.v@gmail.com>
2015-01-02 10:11:17 -0800

    Solution for problem 2.

---
+++ problem2.py
@@ -0,0 +1,38 @@
+Copyright (c) 2014 Chandra Shekhar
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+def prob2():
+    fib=[1,1,2]
+    while True:
+        fib.append(fib[-2]+fib[-1])
+        if fib[-1] > 4*10**6:
+            break
+
+    sum=0
+    print fib
+
+    for i in range(len(fib)):
+        if fib[i]%2==0:
+            sum+=fib[i]
+
+
+    print sum
+
+prob2()
